b36aa52d90a893a42bb13625718038680fede826
WALUNJ <sw0455@att.com>
2016-12-05 01:20:57 -0500

    Module 2 assignment

---
+++ module2-solution/scripts/app.js
@@ -0,0 +1,68 @@
+(function (){
+  'use strict';
+angular.module('ShoppingListCheckOff',[])
+.controller('ToBuyController',ToBuyController)
+.controller('AlreadyBoughtController',AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+
+ToBuyController.$inject= ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService)
+{
+  var tobuy = this;
+
+tobuy.items = ShoppingListCheckOffService.getBuyItems();
+
+tobuy.itembought = function (index)
+{
+  ShoppingListCheckOffService.bought(index);
+};
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService)
+{
+  var bought = this;
+
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService()
+{
+  var service = this;
+
+  var tobuylist = [
+    {
+        name: "Cookies",
+        quantity: "3 bags"
+    },
+    {
+      name: "Sugary Drinks",
+      quantity: "20 bottles"
+    },
+    {
+      name: "Chips",
+      quantity: "2 bags"
+    }];
+
+    var boughtlist = [];
+
+    service.bought = function (itemIndex)
+    {
+    boughtlist.push(tobuylist[itemIndex]);
+    tobuylist.splice(itemIndex,1);
+      console.log( itemIndex);
+    };
+
+    service.getBuyItems =  function ()
+    {
+      return tobuylist;
+    };
+
+    service.getBoughtItems = function()
+    {
+      return boughtlist;
+    };
+
+}
+
+})();
